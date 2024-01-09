78b5d831cb89dfbb66605af1f9d6f01633cde91b
BaneleMlamleli <mlamlelibanele@yahoo.com>
2024-01-10 00:40:13 +0200

    As of Selenium 4.6 if I'm not mistaken, Selenium has a built-in driver manager instead of downloading the drivers manually to your device path or using the WebDriverManager. Selenium Manager automatically discovers, downloads, and caches the browsers driven with Selenium (Chrome, Firefox, and Edge) when these browsers are not installed in the local system.

--- src/main/java/com/swag_labs/core/BaseClass.java
+++ src/main/java/com/swag_labs/core/BaseClass.java
@@ -14,28 +14,29 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 public class BaseClass {
 
     public static WebDriver webDriver = null;
-    static final String DRIVER_PATH = "C:/Users/cipher/Documents/Programming_local/webdrivers/";
+    // static final String DRIVER_PATH = "C:/Users/cipher/Documents/Programming_local/webdrivers/";
     static final String BASE_URL = "https://www.saucedemo.com/";
     final static String BROWSER = "chrome"; //chrome, firefox, edge
     
     public static void launchBrowserWithWebsiteHomePage() {
         switch (BROWSER) {
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", DRIVER_PATH + "chromedriver.exe");
+                // System.setProperty("webdriver.chrome.driver", DRIVER_PATH + "chromedriver.exe");
                 webDriver = new ChromeDriver();
                 webDriver.manage().window().maximize();
                 webDriver.navigate().to(BASE_URL);
                 break;
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", DRIVER_PATH + "geckodriver.exe");
-                FirefoxOptions options = new FirefoxOptions();
-                options.setBinary("C:/Program Files/Mozilla Firefox/firefox.exe");
-                webDriver = new FirefoxDriver(options);
+                // System.setProperty("webdriver.gecko.driver", DRIVER_PATH + "geckodriver.exe");
+                // FirefoxOptions options = new FirefoxOptions();
+                // options.setBinary("C:/Program Files/Mozilla Firefox/firefox.exe");
+                // webDriver = new FirefoxDriver(options);
+                webDriver = new FirefoxDriver();
                 webDriver.manage().window().maximize();
                 webDriver.navigate().to(BASE_URL);
                 break;
             case "edge":
-                System.setProperty("webdriver.edge.driver", DRIVER_PATH + "msedgedriver.exe");
+                // System.setProperty("webdriver.edge.driver", DRIVER_PATH + "msedgedriver.exe");
                 webDriver = new EdgeDriver();
                 webDriver.manage().window().maximize();
                 webDriver.navigate().to(BASE_URL);
